c9e93d534e7f3deed9e4cc4f0d673572816b38f6
Finnegan Hewitt <fbhewitt@gmail.com>
2015-02-23 16:59:57 -0700

    Fixed capybara tests for authozations

--- Gemfile
+++ Gemfile
@@ -44,4 +44,5 @@ group :development, :test do
   gem "database_cleaner"
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'launchy'
 end
--- spec/features/courses_spec.rb
+++ spec/features/courses_spec.rb
@@ -2,19 +2,36 @@ require 'rails_helper'
 
 describe 'users can CRUD Course' do
   scenario 'user can create Course' do
-    visit '/courses'
+    user = User.create(user_name: 'finn', about: 'nuffin', password: "password")
+    visit '/log-in'
+    fill_in 'User name', :with => 'finn'
+    fill_in "Password", :with => 'password'
+    click_on "Sign In"
+    click_on 'Courses'
     click_on 'New Course'
     fill_in 'Title', :with => 'joe'
+    check 'Is this course at night?'
     click_on 'Create Course'
     expect(page).to have_content('joe')
   end
   scenario 'user can view a Course show page' do
-    course = Course.create(title: 'finn')
-    visit "/courses/#{course.id}"
+    user = User.create(user_name: 'finn', about: 'nuffin', password: "password")
+    visit '/log-in'
+    fill_in 'User name', :with => 'finn'
+    fill_in "Password", :with => 'password'
+    click_on "Sign In"
+    course = Course.create(title: 'finn', day_night: true)
+    click_on "Courses"
+    click_on "finn"
     expect(page).to have_content('finn')
   end
   scenario 'user can update a Course' do
-    course = Course.create(title: 'finn')
+    user = User.create(user_name: 'finn', about: 'nuffin', password: "password")
+    visit '/log-in'
+    fill_in 'User name', :with => 'finn'
+    fill_in "Password", :with => 'password'
+    click_on "Sign In"
+    course = Course.create(title: 'finn', day_night: true)
     visit "/courses/#{course.id}"
     click_on "Edit"
     fill_in 'Title', :with => 'change'
@@ -22,7 +39,12 @@ describe 'users can CRUD Course' do
     expect(page).to have_content('change')
   end
   scenario 'user can delete a course' do
-    course = Course.create(title: 'finn')
+    user = User.create(user_name: 'finn', about: 'nuffin', password: "password")
+    visit '/log-in'
+    fill_in 'User name', :with => 'finn'
+    fill_in "Password", :with => 'password'
+    click_on "Sign In"
+    course = Course.create(title: 'finn', day_night: true)
     visit "/courses"
     click_on "Delete"
     expect(page).to have_no_content('finn')
